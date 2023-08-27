1f6a4e8f0739d0d3e5daaac61feba92dd979889d
artm22es <artm22es@gmail.com>
2023-08-27 08:58:47 +0200

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -4,3 +4,10 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 const API_KEY = '39083023-6bd2967288a0046d4cd8dec4e';
 axios.defaults.headers.common['Authorization'] = API_KEY;
 axios.defaults.baseURL = 'https://pixabay.com/api/';
+
+async function fetchPictures() {
+    try {
+        const response = await axios.get('q');
+        console.log(response);
+    }
+}
